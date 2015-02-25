ea7f1d5f39bb4230175740cddd8b9d85d9ef8679
Ryan O'Mullan <omullan.ryan@gmail.com>
2015-02-25 10:36:30 -0300

    add to Heroku

--- DjangoAdMag/DjangoAdMag/wsgi.py
+++ DjangoAdMag/DjangoAdMag/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoAdMag project.
+WSGI config for django_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoAdMag.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
